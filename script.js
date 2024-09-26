088411858430eadcbae8a5cda80e11832aa3eab8
Saksham <156931855+codersam108@users.noreply.github.com>
2024-09-26 22:01:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+$(".h").on("click",function(event){
+    
+      if(!this.dataset.clicked)
+      {
+        $(this).find("img").attr("src","./assets/images/icon-minus.svg");
+        $(this).next("h4").slideDown(300);
+        this.setAttribute("data-clicked","true");
+      }
+    else{
+        $(this).find("img").attr("src","./assets/images/icon-plus.svg");
+        $(this).next("h4").slideUp(300);
+        this.removeAttribute("data-clicked");
+}
+});
